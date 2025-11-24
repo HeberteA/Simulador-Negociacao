bffbea5b4d20dcbc261f0ef01791327ead3035a3
HeberteA <heberte.2307@gmail.com>
2025-11-24 12:10:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ APP_STYLE_CSS = """
     background-color: transparent !important;
     background-image: linear-gradient(160deg, #3b3b3b 0%, #0a0a0c 100%) !important;
     
-    border: 1px solid rgba(255, 255, 255, 0.4) !important;
+    border: 1px solid rgba(255, 255, 255, 0.5) !important;
     border-radius: 16px !important;
     box-shadow: 0 10px 30px rgba(0, 0, 0, 0.7) !important;
     padding: 24px !important;
@@ -38,9 +38,9 @@ APP_STYLE_CSS = """
 }
 .st-key-gradiente_container1 {
     background-color: transparent !important;
-    background-image: linear-gradient(160deg, #242323 0%, #0a0a0c 100%) !important;
+    background-image: linear-gradient(160deg, #3b3b3b 0%, #0a0a0c 100%) !important;
     
-    border: 1px solid rgba(255, 255, 255, 0.4) !important;
+    border: 1px solid rgba(255, 255, 255, 0.5) !important;
     border-radius: 16px !important;
     box-shadow: 0 10px 30px rgba(0, 0, 0, 0.7) !important;
     padding: 24px !important;
@@ -49,9 +49,9 @@ APP_STYLE_CSS = """
 
 .st-key-gradiente_container2 {
     background-color: transparent !important;
-    background-image: linear-gradient(160deg, #242323 0%, #0a0a0c 100%) !important;
+    background-image: linear-gradient(160deg, #3b3b3b 0%, #0a0a0c 100%) !important;
     
-    border: 1px solid rgba(255, 255, 255, 0.4) !important;
+    border: 1px solid rgba(255, 255, 255, 0.5) !important;
     border-radius: 16px !important;
     box-shadow: 0 10px 30px rgba(0, 0, 0, 0.7) !important;
     padding: 24px !important;
@@ -110,9 +110,9 @@ label[data-testid="stLabel"] {
 /* CARD DE RESULTADO (HTML) */
 .lavie-card {
     background-color: transparent !important;
-    background-image: linear-gradient(160deg, #242323 0%, #0a0a0c 100%) !important;
+    background-image: linear-gradient(160deg, #3b3b3b 0%, #0a0a0c 100%) !important;
     
-    border: 1px solid rgba(255, 255, 255, 0.4) !important;
+    border: 1px solid rgba(255, 255, 255, 0.5) !important;
     padding: 30px;
     border-radius: 16px !important;
     box-shadow: 0 15px 40px rgba(0, 0, 0, 0.7) !important;
