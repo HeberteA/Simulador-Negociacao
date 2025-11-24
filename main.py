9d7360342fdbdad3552687261ea98c0a4c1e2d15
HeberteA <heberte.2307@gmail.com>
2025-11-24 10:26:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -254,12 +254,12 @@ with tab1:
         
     with st.container(border=True):
         render_header("calendar_month", "Configuração de Prazos")
-        ent, men, tip, num = st.columns(4)
+        ent, tip, = st.columns(2)
         num_entrada = ent.number_input("Nº Parc. Entrada", min_value=1, step=1, key="main_num_entrada")
-        num_mensal = men.number_input("Nº Parc. Mensais", min_value=0, step=1, key="main_num_mensal")
+        num_mensal = ent.number_input("Nº Parc. Mensais", min_value=0, step=1, key="main_num_mensal")
       
         tipo_intercalada = tip.selectbox("Tipo", ["Semestral", "Trimestral", "Anual", "Bimestral", "Quadrimestral"], key="main_tipo_intercalada")
-        num_intercalada = num.number_input("Nº Parc.", min_value=0, step=1, key="main_num_intercalada")
+        num_intercalada = tip.number_input("Nº Parc.", min_value=0, step=1, key="main_num_intercalada")
 
     st.markdown("<br>", unsafe_allow_html=True)
 
